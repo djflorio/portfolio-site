cc4daad8ac9026fb8aff8b4a7593d7f0c6795e27
Dan Florio <dflo@danflorio.com>
2018-06-11 18:10:59 -0400

    start music section

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import Main from './partials/main/Main';
+import Music from './partials/music/Music';
 
 class App extends Component {
   render() {
-
-    const musicStyle = {
-      marginTop: window.innerHeight
-    };
-
     return (
       <div className="App">
         <Main />
-        <section className="music" style={musicStyle}>
-          Music goes here
-        </section>
+        <Music />
       </div>
     );
   }
---
+++ src/partials/music/Music.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Music.css';
+
+import adaywiser from './img/adaywiser.jpg';
+import bigthoughts from './img/bigthoughts.jpg';
+import malleability from './img/malleability.jpg';
+
+
+class Music extends React.Component {
+  render() {
+    return (
+      <section className="music">
+        <ul className="albums">
+          <li className="album">
+            <img className="album__art" src={adaywiser} />
+          </li>
+          <li className="album">
+            <img className="album__art" src={malleability} />
+          </li>
+          <li className="album">
+            <img className="album__art" src={bigthoughts} />
+          </li>
+        </ul>
+      </section>
+    );
+  }
+}
+
+export default Music;
