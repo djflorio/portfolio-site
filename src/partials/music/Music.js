2ca3f88bbba41057646f43466fc3d627d73f0b43
Dan Florio <dflo@danflorio.com>
2018-06-11 22:41:11 -0400

    fade albums in

--- src/partials/music/Music.js
+++ src/partials/music/Music.js
@@ -7,10 +7,37 @@ import malleability from './img/malleability.jpg';
 
 
 class Music extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.albumRef = React.createRef();
+
+    this.state = {
+      visible: false
+    };
+  }
+
+  componentWillUpdate() {
+    if (this.albumRef.current.offsetTop - this.props.scrollY < 350) {
+      if (!this.state.visible) {
+        this.setState({ visible: true });
+      }
+    } else {
+      if (this.state.visible) {
+        this.setState({ visible: false });
+      }
+    }
+  }
+
   render() {
+
+    const style = {
+      opacity: this.state.visible ? 1 : 0
+    }
+
     return (
-      <section className="music">
-        <ul className="albums">
+      <section className="music" ref={this.albumRef}>
+        <ul className="albums" style={style}>
           <li className="album">
             <img className="album__art" src={adaywiser} />
           </li>
