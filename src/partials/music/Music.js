d1a65ed0deae882acb16d8da692550e9f5b38c07
Dan Florio <dflo@danflorio.com>
2018-06-13 00:05:11 -0400

    change album layout

--- src/partials/music/Music.js
+++ src/partials/music/Music.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import './Music.css';
 
+import Zoom from 'react-reveal/Zoom';
+
 import adaywiser from './img/adaywiser.jpg';
 import bigthoughts from './img/bigthoughts.jpg';
 import malleability from './img/malleability.jpg';
+
+
+class Album extends React.Component {
+  render() {
+    return (
+      <Zoom>
+      <li className="album">
+        <div className="album__header">
+          <img className="album__art" src={this.props.art} />
+          <span className="album__info">
+            <h2 className="album__title">{this.props.title}</h2>
+            <h3 className="album__year">{this.props.year}</h3>
+          </span>
+        </div>
+      </li>
+      </Zoom>
+    );
+  }
+}
 
 
 class Music extends React.Component {
@@ -13,15 +34,21 @@ class Music extends React.Component {
     return (
       <section className="music" ref={this.albumRef}>
         <ul className="albums">
-          <li className="album">
-            <img className="album__art" src={adaywiser} />
-          </li>
-          <li className="album">
-            <img className="album__art" src={malleability} />
-          </li>
-          <li className="album">
-            <img className="album__art" src={bigthoughts} />
-          </li>
+          <Album
+            art={adaywiser}
+            title="A Day Wiser"
+            year="2017"
+          />
+          <Album
+            art={malleability}
+            title="Malleability"
+            year="2014"
+          />
+          <Album
+            art={bigthoughts}
+            title="Big Thoughts in a Small Place"
+            year="2012"
+          />
         </ul>
       </section>
     );
