6442a787354f945ab2c4aee8bc53681187c6fcf7
Dan Florio <dflo@danflorio.com>
2018-06-14 16:53:32 -0400

    add dummy data

--- src/partials/music/Music.js
+++ src/partials/music/Music.js
@@ -8,25 +8,6 @@ import bigthoughts from './img/bigthoughts.jpg';
 import malleability from './img/malleability.jpg';
 
 
-class Album extends React.Component {
-  render() {
-    return (
-      <Zoom>
-      <li className="album">
-        <div className="album__header">
-          <img className="album__art" src={this.props.art} />
-          <span className="album__info">
-            <h2 className="album__title">{this.props.title}</h2>
-            <h3 className="album__year">{this.props.year}</h3>
-          </span>
-        </div>
-      </li>
-      </Zoom>
-    );
-  }
-}
-
-
 class Music extends React.Component {
 
   render() {
