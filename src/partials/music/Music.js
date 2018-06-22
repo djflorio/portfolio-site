1ff0ca78b9b9e235d16f39641623ed235a6ea277
Dan Florio <dflo@danflorio.com>
2018-06-22 17:23:53 -0400

    remove return

--- src/partials/music/Music.js
+++ src/partials/music/Music.js
@@ -11,84 +11,82 @@ import Zoom from 'react-reveal/Zoom';
 import Fade from 'react-reveal/Fade';
 
 
-const Music = (props) => {
-  return (
-    <section className="music">
-      <div className="music__content">
-        <Zoom cascade>
-          <ul className="albums">
+const Music = (props) => (
+  <section className="music">
+    <div className="music__content">
+      <Zoom cascade>
+        <ul className="albums">
+          {
+            props.albums.map(album => (
+              <li
+                key={album.id}
+                className={classnames(
+                  "album",
+                  {"album--open": props.currentAlbum === album},
+                  {"album--playing": props.currentAlbum === album && props.playing}
+                )}
+                onClick={()=>props.onAlbumClick(album)}
+              >
+                <img
+                  className="album__art"
+                  src={album.art}
+                  alt={album.title}
+                />
+                <img
+                  className="album__record"
+                  src={album.record}
+                  alt="record"
+                />
+              </li>
+            ))
+          }
+        </ul>
+      </Zoom>
+      {
+        props.currentAlbum !== null &&
+        <div className="music__info">
+          <div className="music__header">
+            <h1 className="music__title">{props.currentAlbum.title}</h1>
+            <h2 className="music__year">{props.currentAlbum.year}</h2>
+          </div>
+          <div className="music__links">
             {
-              props.albums.map(album => (
+              props.currentAlbum.links.map(link => (
+                <a
+                  key={link.id}
+                  className="music__link"
+                  href={link.url}
+                  target="_blank">
+                  {link.name}
+                </a>
+              ))
+            }
+          </div>
+          <Fade cascade>
+          <ul className="music__tracks">
+            {
+              props.currentAlbum.tracks.map(track => (
                 <li
-                  key={album.id}
+                  key={track.id}
                   className={classnames(
-                    "album",
-                    {"album--open": props.currentAlbum === album},
-                    {"album--playing": props.currentAlbum === album && props.playing}
+                    "music__track",
+                    {"music__track--playing": props.currentSong === track}
                   )}
-                  onClick={()=>props.onAlbumClick(album)}
-                >
-                  <img
-                    className="album__art"
-                    src={album.art}
-                    alt={album.title}
+                  onClick={()=>props.onSongClick(track)}>
+                  <FontAwesomeIcon
+                    className="music__icon"
+                    icon={props.playing && props.currentSong === track ? faPause : faPlay}
                   />
-                  <img
-                    className="album__record"
-                    src={album.record}
-                    alt="record"
-                  />
+                  {track.title}
                 </li>
               ))
             }
           </ul>
-        </Zoom>
-        {
-          props.currentAlbum !== null &&
-          <div className="music__info">
-            <div className="music__header">
-              <h1 className="music__title">{props.currentAlbum.title}</h1>
-              <h2 className="music__year">{props.currentAlbum.year}</h2>
-            </div>
-            <div className="music__links">
-              {
-                props.currentAlbum.links.map(link => (
-                  <a
-                    key={link.id}
-                    className="music__link"
-                    href={link.url}
-                    target="_blank">
-                    {link.name}
-                  </a>
-                ))
-              }
-            </div>
-            <Fade cascade>
-            <ul className="music__tracks">
-              {
-                props.currentAlbum.tracks.map(track => (
-                  <li
-                    key={track.id}
-                    className={classnames(
-                      "music__track",
-                      {"music__track--playing": props.currentSong === track}
-                    )}
-                    onClick={()=>props.onSongClick(track)}>
-                    <FontAwesomeIcon
-                      className="music__icon"
-                      icon={props.playing && props.currentSong === track ? faPause : faPlay}
-                    />
-                    {track.title}
-                  </li>
-                ))
-              }
-            </ul>
-            </Fade>
-          </div>
-        }
-      </div>
-    </section>
-  );
-}
+          </Fade>
+        </div>
+      }
+    </div>
+  </section>
+);
 
 export default Music;
