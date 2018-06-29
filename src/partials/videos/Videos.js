784419b18a9f39b87cf196f76e927914a4e53890
Dan Florio <dflo@danflorio.com>
2018-06-28 20:46:34 -0400

    make videos section cool

--- src/partials/videos/Videos.js
+++ src/partials/videos/Videos.js
@@ -3,11 +3,20 @@ import './Videos.css';
 
 import placeholder from './img/16x9.png';
 import tree from './img/tree.png';
+import tree2 from './img/tree2.png';
 import Fade from 'react-reveal/Fade';
 
 const Videos = (props) => (
   <section className="videos">
-    <img className="videos__tree" src={tree} />
+    <span className="videos__header">
+      <img className="videos__tree" src={tree} />
+      <img className="videos__tree2" src={tree2} />
+      <Fade right>
+      <h1 className="videos__header-text">
+        videos
+      </h1>
+      </Fade>
+    </span>
     <span className="videos__bg-overlay"></span>
     <ul className="videos__list">
     {
